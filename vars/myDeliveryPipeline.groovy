8a08ce5887bcac27430396816d072c8f59bba746
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2024-01-28 16:03:12 -0300

    Add test

---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,18 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent any
+        stages {
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+        }
+    }
+}
