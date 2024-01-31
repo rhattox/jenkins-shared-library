da2a84a5a98afdf6ef7dcaa7c051e7f0ef286201
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2024-01-30 21:21:49 -0300

    attemtp to fix pipeline

--- vars/buildJs.groovy
+++ vars/buildJs.groovy
@@ -3,13 +3,14 @@
 import org.library.sayHello
 
 def call(String name = 'human') {
+    notification = new sayHello()
+
     pipeline {
         agent any
         stages {
             stage('Build') {
                 steps {
-                    cho "Bye, ${name}."
-                    def notification = new sayHello()
+                    echo "Bye, ${name}."
                     notification 'Zina'
                 }
             }
