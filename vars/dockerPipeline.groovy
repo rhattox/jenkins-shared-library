ebeff69e5b8b7877096a241967af35c485f59ba9
Bruno Souto <65253099+rhattox@users.noreply.github.com>
2024-02-06 21:57:57 -0300

    Add new pipeline

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,16 +1,14 @@
 #!/usr/bin/env groovy
 
-import org.library.dockerBuildStage
-
 def call(args) {
-    dockerBuildStageFunc = new dockerBuildStage()
+    dockerBuildStageFunc = new org.library.dockerBuildStage()
 
     pipeline {
         agent any
         stages {
             stage('Build') {
                 steps {
-                    dockerBuildStageFunc '5555'
+                    dockerBuildStageFunc.buildDockerStage('5555')
                 }
             }
         }
